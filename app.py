4cdb3ed0d7f0d13be3bea540b24495f200918cdd
Harsh Patel <harshp4106@gmail.com>
2023-09-27 12:03:37 +0530

    streamlit added

--- app.py
+++ app.py
@@ -1,78 +1,12 @@
-from langchain import PromptTemplate
-from langchain.chains import RetrievalQA
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.document_loaders import PyPDFLoader , DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.llms import CTransformers
-import sys
+import streamlit as st
+from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import google.generativeai as palm
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.llms import GooglePalm
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+import os
 
-
-#**Step 1: Load the PDF File from Data Path****
-loader=DirectoryLoader('Data/',
-                       glob="*.pdf",
-                       loader_cls=PyPDFLoader)
-
-documents=loader.load()
-
-
-#***Step 2: Split Text into Chunks***
-
-text_splitter=RecursiveCharacterTextSplitter(
-                                             chunk_size=500,
-                                             chunk_overlap=50)
-
-
-text_chunks=text_splitter.split_documents(documents)
-
-print(f"Total text chunks created are: {len(text_chunks)}")
-#**Step 3: Load the Embedding Model***
-
-
-embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device':'cpu'})
-
-
-#**Step 4: Convert the Text Chunks into Embeddings and Create a FAISS Vector Store***
-vector_store=FAISS.from_documents(text_chunks, embeddings)
-
-
-#**Step 5: Find the Top 3 Answers for the Query***
-
-query="ResNet outperforms which models"
-docs = vector_store.similarity_search(query)
-
-llm=CTransformers(model="models\llama-2-7b-chat.ggmlv3.q4_0.bin",
-                  model_type="llama",
-                  config={'max_new_tokens':128,
-                          'temperature':0.01})
-
-
-template="""Use the following pieces of information to answer the user's question.
-If you dont know the answer just say you know, don't try to make up an answer.
-
-Context:{context}
-Question:{question}
-
-Only return the helpful answer below and nothing else
-Helpful answer
-"""
-
-qa_prompt=PromptTemplate(template=template, input_variables=['context', 'question'])
-
-chain = RetrievalQA.from_chain_type(llm=llm,
-                                   chain_type='stuff',
-                                   retriever=vector_store.as_retriever(search_kwargs={'k': 2}),
-                                   return_source_documents=True,
-                                   chain_type_kwargs={'prompt': qa_prompt})
-
-while True:
-    user_input=input(f"prompt:")
-    if query=='exit':
-        print('Exiting')
-        sys.exit()
-    if query=='':
-        continue
-    result=chain({'query':user_input})
-    print(f"Answer:{result['result']}")
-    print("\n")
